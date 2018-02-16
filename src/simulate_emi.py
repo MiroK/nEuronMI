f4f2a0f8239052c904691e5f01103b9032beef85
Alessio Buccino <alejoe9187@gmail.com>
2018-02-16 10:58:54 -0800

    save neuron mesh for currents

--- src/simulate_emi.py
+++ src/simulate_emi.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
                       'cond_int': 7.0,                   # mS/cm^2
                       'cond_ext': 3.0,                   # mS/cm^2
                       'I_ion': 0.0,
-                      'Tstop': 1.}                       # ms
+                      'Tstop': 10.}                       # ms
     solver_params = {'dt_fem': 1E-2, #1E-3,              # ms
                      'dt_ode': 1E-2, #1E-3,               # ms
                      'linear_solver': 'direct'}
@@ -84,6 +84,7 @@ if __name__ == '__main__':
     areas = {tag: assemble(1 * dx_(tag)) for tag in range(1, 4)}
 
     I_proxy = None
+    is_neuron_mesh = False
 
     v_probe = []
     v_soma = []
@@ -91,13 +92,19 @@ if __name__ == '__main__':
     i_m = []
 
     p_x, p_y, p_z = rec_sites[0]
-    soma_m = [15*conv, 0, 0]
+    soma_m = [7.5*conv, 0, 0]
 
     # Do something with the solutions
     for n, (t, u, current, system_size) in enumerate(stream):
 
         if I_proxy is None:
             I_proxy = snap_to_nearest(current)
+
+        # Store the neuron mesh once for post-processing
+        if not is_neuron_mesh:
+            with HDF5File(mpi_comm_world(), join('results', mesh_root, 'neuron_mesh.h5'), 'w') as nm_out:
+                nm_out.write(current.function_space().mesh(), 'mesh')
+                is_neuron_mesh = True
 
         # msg = 'Normalized curent in %s = %g'
         # for tag, domain in ((1, 'soma'), (2, 'axon'), (3, 'dendrite')):
@@ -122,7 +129,6 @@ if __name__ == '__main__':
     print 'Elapsed time = ', t_stop - t_start
 
     performance.update({'system size': system_size, 'time': processing_time})
-
     v_probe = np.transpose(np.array(v_probe))
 
     np.save(join('results', mesh_root, 'times'), times)
