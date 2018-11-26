3664c533f0a428d3fc827b5b06ea8f088d3c41b9
Alessio Buccino <alejoe9187@gmail.com>
2018-11-26 12:02:08 +0100

    load mapping and save properly probe mapping

--- src/probe_map/probe_correction.py
+++ src/probe_map/probe_correction.py
@@ -2,13 +2,7 @@
 # to mesh generation and finally solution. In a realistic workflow
 # the 3 steps are done separately.
 
-# from mesh.simple_geometry.shapes import (SphereNeuron, MainenNeuron,
-#                                          FancyProbe, PixelProbe)
-# from mesh.simple_geometry.geogen import geofile
-# from mesh.msh_convert import convert
 
-# from solver.neuron_solver import neuron_solver
-# from solver.aux import snap_to_nearest
 import sys
 sys.path.append('../solver')
 from aux import load_mesh
@@ -24,10 +18,12 @@ import subprocess, os
 import matplotlib.pyplot as plt
 
 conv = 1E-4
-probe_map_folder = '/media/terror/code/source/nEuronMI/src/probe_map/results/noneuron_fancy_0_0_-100_coarse_2_box_6_rot_0_rad_0_wprobe/point'
+probe_map_folder = '/home/alessio/Documents/Codes/nEuronMI/src/probe_map/simulations/' \
+                   'noneuron_fancy_0_0_-100_coarse_2_box_6_rot_0_rad_0_wprobe/point'
 probe_map_mesh = join(probe_map_folder, 'u_ext.h5')
 probe_map_elec = join(probe_map_folder, 'elec_dict.npy')
-mesh_path = '/media/terror/code/source/nEuronMI/src/probe_map/results/noneuron_fancy_0_0_-100_coarse_2_box_6_rot_0_rad_0/noneuron_fancy_0_0_-100_coarse_2_box_6_rot_0_rad_0_wprobe.h5'
+mesh_path = '/home/alessio/Documents/Codes/nEuronMI/src/probe_map/meshes/' \
+            'noneuron_fancy_0_0_-100_coarse_2_box_6_rot_0_rad_0/noneuron_fancy_0_0_-100_coarse_2_box_6_rot_0_rad_0_wprobe.h5'
 mesh, surfaces, volumes, aux_tags = load_mesh(mesh_path)
 
 
