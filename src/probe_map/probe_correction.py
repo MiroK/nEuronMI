e8fdaec8ef165f2bc54495dab91fcaa9a5ac5e3d
pierre berthet <berthetp@gmail.com>
2018-12-17 14:27:06 +0100

    now takes a folder as argument, display intra and inter modality comparisons

--- src/probe_map/probe_correction.py
+++ src/probe_map/probe_correction.py
@@ -14,13 +14,16 @@ from os.path import join
 import time
 import seaborn as sns
 from neuroplot import *
-
+import sys
+from glob import glob
 # Shut fenics info up
 set_log_level(WARNING)
 
 import subprocess, os
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import MEAutility as mea
+
 
 def order_recording_sites(sites1, sites2):
     order = []
@@ -29,6 +32,7 @@ def order_recording_sites(sites1, sites2):
         order.append(np.argmin(distances))
 
     return np.array(order)
+
 
 def monopolar(I, elec, pos, den=4):
     '''
@@ -44,41 +48,78 @@ def monopolar(I, elec, pos, den=4):
     -------
 
     '''
-    return np.array([I / (den*np.pi*0.3*np.linalg.norm(elec - p)*1e4) for p in pos]) * 1000
+    return np.array([I / (den * np.pi * 0.3 * np.linalg.norm(elec - p) * 1e4) for p in pos]) * 1000
 
+
+source_folder = sys.argv[1]
+fold_name = source_folder.split('/')[-2] if source_folder[-1] == '/' else source_folder.split('/')[1]
 
 run_pc = False
 conv = 1E-4
 
-probe_map_folder = '/home/alessio/Documents/Codes/nEuronMI/src/probe_map/simulations/' \
-                   'noneuron_fancy_0_0_-100_coarse_2_box_6_rot_0_rad_0_wprobe/point'
-probe_map_mesh = join(probe_map_folder, 'u_ext.h5')
-probe_map_elec = join(probe_map_folder, 'elec_dict.npy')
-mesh_path = '/home/alessio/Documents/Codes/nEuronMI/src/probe_map/meshes/' \
-            'noneuron_fancy_0_0_-100_coarse_2_box_6_rot_0_rad_0/' \
-            'noneuron_fancy_0_0_-100_coarse_2_box_6_rot_0_rad_0_wprobe.h5'
+probe_map_folder = '/media/terror/code/source/nEuronMI/src/probe_map/results/' \
+                   'noneuron_fancy_0_0_-100_coarse_2_box_5_rot_0_rad_0_wprobe/point'
 
+probe_map_folder_original = '/media/terror/code/source/nEuronMI/src/probe_map/results/' \
+                            'noneuron_fancy_0_0_-100_coarse_2_box_6_rot_0_rad_0_wprobe/point'
+
+probe_map_mesh = join(probe_map_folder, 'u_ext.h5')
+probe_map_elec = join(probe_map_folder_original, 'elec_dict.npy')
+# mesh_path = '/media/terror/code/source/nEuronMI/src/probe_map/results/' \
+#             'noneuron_fancy_0_0_-100_coarse_2_box_6_rot_0_rad_0/' \
+#             'noneuron_fancy_0_0_-100_coarse_2_box_6_rot_0_rad_0_wprobe.h5'
+mesh_path = '/media/terror/code/source/nEuronMI/src/probe_map/results/' \
+            'noneuron_fancy_0_0_-100_coarse_2_box_5_rot_0_rad_0/' \
+            'noneuron_fancy_0_0_-100_coarse_2_box_5_rot_0_rad_0_wprobe.h5'
 load_all_meshes = True
 
 # load neuron
-i_mem = np.loadtxt('bas_imem.txt')
-v_ext_bas = np.loadtxt('bas_vext.txt')
-v_ext_hybrid = np.loadtxt('v_ext_hybrid.txt')
-v_ext_corr = np.loadtxt('v_ext_corr.txt')
-seg_pos = np.loadtxt('seg_pos.txt').T * conv
-pos = np.loadtxt('fem_pos.txt')
+i_mem = np.loadtxt(glob(join(source_folder, 'bas_imem*.txt'))[0])
+v_ext_bas = np.loadtxt(glob(join(source_folder, 'bas_vext*.txt'))[0])
+v_ext_hybrid = np.loadtxt(glob(join(source_folder, 'v_ext_hybrid*.txt'))[0])
+v_ext_corr = np.loadtxt(glob(join(source_folder, 'v_ext_corr*.txt'))[0])
+seg_pos = np.loadtxt(glob(join(source_folder, 'seg_pos*.txt'))[0]).T * conv
+pos = np.loadtxt(glob(join(source_folder, 'fem_pos*.txt'))[0])
+new_pos = []
+pos[:, 1] = np.round(pos[:, 1], decimals=5)
+pos_back = pos
+for u in np.unique(pos[:, 1]):
+    for o in pos[pos[:, 1] == u][pos[pos[:, 1] == u][:, 2].argsort()]:
+        new_pos.append(o)
+pos = np.asarray(new_pos)
 
 elec_dict = np.load(probe_map_elec).item()
-info_mea = {'electrode_name': 'nn_emi', 'pos': pos, 'center': False}
+info_mea = {'electrode_name': 'nn_emi', 'pos': pos_back, 'center': False}
 nn = mea.return_mea(info=info_mea)
 
-no_mesh = '../results/mainen_fancy_40_0_-100_coarse_2_box_5_noprobe'
-w_mesh = '../results/mainen_fancy_40_0_-100_coarse_2_box_5_wprobe'
+# no_mesh = '../results/mainen_fancy_40_0_-100_coarse_2_box_5_noprobe'
+# w_mesh = '../results/mainen_fancy_40_0_-100_coarse_2_box_5_wprobe'
+# mainen_fancy_40_0_-100_coarse_2_box_5_noprobe
+emi_sites = (np.load(join('/media/terror/code/source/nEuronMI/src/mesh/simple_geometry/fancy/mainen_fancy_40_0_-100_coarse_0_box_3_noprobe', 'sites.npy')) - [40 * conv, 0, 0]) / conv
 
-emi_sites = (np.load(join(no_mesh, 'sites.npy')) - [40*conv, 0, 0]) / conv
+new_emis = []
+emi_sites[:, 1] = np.round(emi_sites[:, 1], decimals=5)
+emi_sites_back = emi_sites
+for u in np.unique(emi_sites[:, 1]):
+    for o in emi_sites[emi_sites[:, 1] == u][emi_sites[emi_sites[:, 1] == u][:, 2].argsort()]:
+        new_emis.append(o)
+emi_sites = np.asarray(new_emis)
+
+nat_to_back = []
+for i, p in enumerate(pos):
+    for j, pb in enumerate(pos_back):
+        if (pb == p).all():
+            nat_to_back.append(j)
+nat_to_back = np.asarray(nat_to_back)
+
+
 order = order_recording_sites(pos, emi_sites)
-v_ext_emi_noprobe = np.load(join(no_mesh, 'v_probe.npy'))[order]*1000
-v_ext_emi_wprobe = np.load(join(w_mesh, 'v_probe.npy'))[order]*1000
+order_back = order_recording_sites(pos_back, emi_sites_back)
+# v_ext_emi_noprobe = np.load(join(source_folder, 'v_ext_emi_noprobe.npy'))[order_back] * 1000
+# v_ext_emi_wprobe = np.load(join(source_folder, 'v_ext_emi_wprobe.npy'))[order_back] * 1000
+v_ext_emi_noprobe = np.load(join(source_folder, 'v_ext_emi_noprobe.npy'))[order] * 1000
+v_ext_emi_wprobe = np.load(join(source_folder, 'v_ext_emi_wprobe.npy'))[order] * 1000
+
 info_mea = {'electrode_name': 'nn_emi', 'pos': emi_sites, 'center': False}
 emi_nn = mea.return_mea(info=info_mea)
 
@@ -132,7 +173,7 @@ if run_pc:
 
 # scaling = np.max(np.abs(v_ext_bas))/np.max(np.abs(v_ext_corr))
 # v_ext = np.array([v_ext_corr, v_ext_bas, v_ext_bas*1.8])
-v_ext = np.array([v_ext_hybrid, v_ext_bas, v_ext_bas*2, v_ext_corr])
+v_ext = np.array([v_ext_hybrid, v_ext_bas, v_ext_bas * 2, v_ext_corr])
 v_emi_hybrid = np.array([v_ext_hybrid, v_ext_emi_wprobe])
 v_bas_emi_noprobe = np.array([v_ext_bas, v_ext_emi_noprobe])
 
@@ -178,24 +219,24 @@ ax3.legend(labels=['BAS', 'EMI no probe'], fontsize=18, loc='upper right', ncol=
 # plt.matshow(v_probe)
 
 # ratios
-ratio_bas_hyb = np.max(np.abs(v_ext_bas), axis=1) / np.max(np.abs(v_ext_hybrid), axis=1)
-ratio_moi_hyb = np.max(np.abs(2*v_ext_bas), axis=1) / np.max(np.abs(v_ext_hybrid), axis=1)
-ratio_moi18_hyb = np.max(np.abs(1.8*v_ext_bas), axis=1) / np.max(np.abs(v_ext_hybrid), axis=1)
-ratio_corr_hyb = np.max(np.abs(v_ext_corr), axis=1) / np.max(np.abs(v_ext_hybrid), axis=1)
-ratio_corr_emi = np.max(np.abs(v_ext_corr), axis=1) / np.max(np.abs(v_ext_emi_wprobe), axis=1)
-ratio_bas_emi = np.max(np.abs(v_ext_bas), axis=1) / np.max(np.abs(v_ext_emi_wprobe), axis=1)
-ratio_moi18_emi = np.max(np.abs(1.8*v_ext_bas), axis=1) / np.max(np.abs(v_ext_emi_wprobe), axis=1)
-ratio_moi_emi = np.max(np.abs(2*v_ext_bas), axis=1) / np.max(np.abs(v_ext_emi_wprobe), axis=1)
+ratio_bas_hyb = np.max(np.abs(v_ext_bas[:,150:350]), axis=1) / np.max(np.abs(v_ext_hybrid[:,150:350]), axis=1)
+ratio_moi_hyb = np.max(np.abs(2 * v_ext_bas[:,150:350]), axis=1) / np.max(np.abs(v_ext_hybrid[:,150:350]), axis=1)
+ratio_moi18_hyb = np.max(np.abs(1.8 * v_ext_bas[:,150:350]), axis=1) / np.max(np.abs(v_ext_hybrid[:,150:350]), axis=1)
+ratio_corr_hyb = np.max(np.abs(v_ext_corr[:,150:350]), axis=1) / np.max(np.abs(v_ext_hybrid[:,150:350]), axis=1)
+ratio_corr_emi = np.max(np.abs(v_ext_corr[:,150:350]), axis=1) / np.max(np.abs(v_ext_emi_wprobe[:,150:350]), axis=1)
+ratio_bas_emi = np.max(np.abs(v_ext_bas[:,150:350]), axis=1) / np.max(np.abs(v_ext_emi_wprobe[:,150:350]), axis=1)
+ratio_moi18_emi = np.max(np.abs(1.8 * v_ext_bas[:,150:350]), axis=1) / np.max(np.abs(v_ext_emi_wprobe[:,150:350]), axis=1)
+ratio_moi_emi = np.max(np.abs(2 * v_ext_bas[:,150:350]), axis=1) / np.max(np.abs(v_ext_emi_wprobe[:,150:350]), axis=1)
 fig = plt.figure()
 
-ax22 = fig.add_subplot(1,2,1)
+ax22 = fig.add_subplot(1, 2, 1)
 sns.distplot(ratio_bas_hyb, bins=20, hist=False, rug=True, label='BAS', ax=ax22)
 sns.distplot(ratio_moi_hyb, bins=20, hist=False, rug=True, label='MoI', ax=ax22)
 sns.distplot(ratio_moi18_hyb, bins=20, hist=False, rug=True, label='1.8MoI', ax=ax22)
 ax22.set_title('Peak ratio with  hybrid')
 ax22.legend(fontsize=18, loc='upper right')
 
-ax11 = fig.add_subplot(1,2,2)
+ax11 = fig.add_subplot(1, 2, 2)
 sns.distplot(ratio_bas_emi, bins=20, hist=False, rug=True, label='BAS', ax=ax11)
 sns.distplot(ratio_corr_emi, bins=20, hist=False, rug=True, label='PC', ax=ax11)
 sns.distplot(ratio_moi_emi, bins=20, hist=False, rug=True, label='MoI', ax=ax11)
@@ -221,3 +262,121 @@ print 'difference wprobe: ', np.min(v_ext_emi_wprobe) - np.min(v_ext_hybrid)
 print 'peak MoI: ', np.min(2*v_ext_bas)
 print 'difference wprobe: ', np.min(v_ext_emi_wprobe) - np.min(2*v_ext_bas)
 
+
+
+
+
+plt.figure(figsize=[15, 10])
+plt.suptitle('Paired comparison left Vs right electrodes, intra modality [{}]'.format(fold_name))
+ax_1 = plt.subplot(121)
+
+# plt.title('BAS and Hybrid')
+norm = mpl.colors.Normalize(0, 9)
+# col = (cell.vmem.T[spike_time_loc[0]] + 100) / 150.
+# col = {'soma': 'k', 'axon': 'b', 'dendrite': 'r', }
+# norm = mpl.colors.Normalize(0, n_sec)
+set2 = plt.get_cmap('tab10')
+colr = plt.cm.ScalarMappable(norm=norm, cmap=set2)
+for i in range(10):
+    ax_1.plot(v_ext_bas[nat_to_back[i]], color=colr.to_rgba(i), linestyle='-', label='electrode {}'.format(i))
+    ax_1.plot(v_ext_bas[nat_to_back[i + 22]], color=colr.to_rgba(i), linestyle='--', label='electrode {}'.format(i + 22))
+ax_1.legend()
+ax_1.set_title('BAS')
+
+ax_2 = plt.subplot(122)
+# plt.title('Hybrid')
+norm = mpl.colors.Normalize(0, 9)
+# col = (cell.vmem.T[spike_time_loc[0]] + 100) / 150.
+# col = {'soma': 'k', 'axon': 'b', 'dendrite': 'r', }
+# norm = mpl.colors.Normalize(0, n_sec)
+set2 = plt.get_cmap('tab10')
+colr = plt.cm.ScalarMappable(norm=norm, cmap=set2)
+for i in range(10):
+    ax_2.plot(v_ext_hybrid[nat_to_back[i]], color=colr.to_rgba(i), linestyle='-', label='electrode {}'.format(i))
+    ax_2.plot(v_ext_hybrid[nat_to_back[i + 22]], color=colr.to_rgba(i), linestyle='--', label='electrode {}'.format(i + 22))
+# ax_2.legend()
+ax_2.set_title('Hybrid')
+
+
+ymin = np.min([np.min(v_ext_bas), np.min(v_ext_emi_wprobe)])
+ymax = np.max([np.max(v_ext_bas), np.max(v_ext_emi_wprobe)])
+plt.figure(figsize=[20, 20])
+plt.suptitle('Paired comparison of largest inter modality differences per electrode [{}]'.format(fold_name))
+ax_a = plt.subplot(221)
+n_elec = 4
+norm = mpl.colors.Normalize(0, n_elec)
+set2 = plt.get_cmap('tab10')
+colr = plt.cm.ScalarMappable(norm=norm, cmap=set2)
+ax_a.set_title('BAS - EMI')
+temp = ratio_moi_emi.argsort()
+for ix, i in enumerate(temp[-n_elec:]):
+    natural_electrode = np.where(nat_to_back == i)[0][0]
+    ax_a.plot(v_ext_bas[i], color=colr.to_rgba(ix), linestyle='-', label='electrode {}'.format(natural_electrode))
+    ax_a.plot(v_ext_emi_wprobe[i], color=colr.to_rgba(ix), linestyle='--', label='electrode {}'.format(natural_electrode))  # emi same electrode data
+    ax_a.plot(v_ext_bas[i] - v_ext_emi_wprobe[i], color=colr.to_rgba(ix), linestyle=':', label='difference')
+# ratio_moi18_emi
+ax_a.legend()
+
+
+ax_b = plt.subplot(222)
+n_elec = 4
+norm = mpl.colors.Normalize(0, n_elec)
+set2 = plt.get_cmap('tab10')
+colr = plt.cm.ScalarMappable(norm=norm, cmap=set2)
+ax_b.set_title('MOI - EMI')
+temp = ratio_bas_emi.argsort()
+for ix, i in enumerate(temp[-n_elec:]):
+    natural_electrode = np.where(nat_to_back == i)[0][0]
+    ax_b.plot(2 * v_ext_bas[i], color=colr.to_rgba(ix), linestyle='-', label='electrode {}'.format(natural_electrode))
+    ax_b.plot(v_ext_emi_wprobe[i], color=colr.to_rgba(ix), linestyle='--', label='electrode {}'.format(natural_electrode))  # emi same electrode data
+    ax_b.plot(2 * v_ext_bas[i] - v_ext_emi_wprobe[i], color=colr.to_rgba(ix), linestyle=':', label='difference')
+# ratio_moi18_emi
+ax_b.legend()
+ymin_t = np.min([np.min(2 * v_ext_bas), np.min(v_ext_emi_wprobe)])
+ymax_t = np.max([np.max(2 * v_ext_bas), np.max(v_ext_emi_wprobe)])
+if ymin_t < ymin:
+    ymin = ymin_t
+if ymax_t > ymax:
+    ymax = ymax_t
+
+
+
+ax_c = plt.subplot(223)
+n_elec = 4
+norm = mpl.colors.Normalize(0, n_elec)
+set2 = plt.get_cmap('tab10')
+colr = plt.cm.ScalarMappable(norm=norm, cmap=set2)
+ax_c.set_title('Corr - Hybrid')
+temp = ratio_corr_hyb.argsort()
+for ix, i in enumerate(temp[-n_elec:]):
+    natural_electrode = np.where(nat_to_back == i)[0][0]
+    ax_c.plot(v_ext_corr[i], color=colr.to_rgba(ix), linestyle='-', label='electrode {}'.format(natural_electrode))
+    ax_c.plot(v_ext_hybrid[i], color=colr.to_rgba(ix), linestyle='--', label='electrode {}'.format(natural_electrode))  # emi same electrode data
+    ax_c.plot(v_ext_corr[i] - v_ext_hybrid[i], color=colr.to_rgba(ix), linestyle=':', label='difference')
+# ratio_moi18_emi
+ax_c.legend()
+ymin_t = np.min([np.min(v_ext_corr), np.min(v_ext_hybrid)])
+ymax_t = np.max([np.max(v_ext_corr), np.max(v_ext_hybrid)])
+if ymin_t < ymin:
+    ymin = ymin_t
+if ymax_t > ymax:
+    ymax = ymax_t
+
+ax_d = plt.subplot(224)
+n_elec = 4
+norm = mpl.colors.Normalize(0, n_elec)
+set2 = plt.get_cmap('tab10')
+colr = plt.cm.ScalarMappable(norm=norm, cmap=set2)
+ax_d.set_title('PC - EMI')
+temp = ratio_corr_emi.argsort()
+for ix, i in enumerate(temp[-n_elec:]):
+    natural_electrode = np.where(nat_to_back == i)[0][0]
+    ax_d.plot(v_ext_corr[i], color=colr.to_rgba(ix), linestyle='-', label='electrode {}'.format(natural_electrode))
+    ax_d.plot(v_ext_emi_wprobe[i], color=colr.to_rgba(ix), linestyle='--', label='electrode {}'.format(natural_electrode))  # emi same electrode data
+    ax_d.plot(v_ext_corr[i] - v_ext_emi_wprobe[i], color=colr.to_rgba(ix), linestyle=':', label='difference')
+# ratio_moi18_emi
+ax_d.legend()
+ax_d.set_ylim([ymin, ymax])
+ax_c.set_ylim([ymin, ymax])
+ax_b.set_ylim([ymin, ymax])
+ax_a.set_ylim([ymin, ymax])
