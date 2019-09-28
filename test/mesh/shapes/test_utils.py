809fbd54cd78e3d7f8ba555e3ed7e317973bcb11
Miroslav Kuchta <miroslav.kuchta@gmail.com>
2019-09-28 16:19:50 +0200

    mockup testing

---
+++ test/mesh/shapes/test_utils.py
@@ -0,0 +1,11 @@
+from nEuronMI.mesh.shapes.utils import *
+import unittest
+
+
+class TestMeshUtils(unittest.TestCase):
+
+    def test_find_first(self):
+        my = find_first(4, range(19))
+        truth = list(range(19)).index(4)
+
+        self.assertTrue(my == truth)
